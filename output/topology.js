eab79536c608abf81bb9546b47c970e47f2a5d9b
tomok0823 <t-imai@ist.osaka-u.ac.jp>
2016-12-14 03:47:29 +0900

    add picture

--- output/topology.js
+++ output/topology.js
@@ -7,29 +7,5 @@ edges = [];
 var DIR = './images/';
 // Create a data table with nodes.
 
-nodes.push({id: 2, label: '0x2', image:DIR+'switch.jpg', shape: 'image'});
-
-nodes.push({id: 3, label: '0x3', image:DIR+'switch.jpg', shape: 'image'});
-
-nodes.push({id: 4, label: '0x4', image:DIR+'switch.jpg', shape: 'image'});
-
-nodes.push({id: 6, label: '0x6', image:DIR+'switch.jpg', shape: 'image'});
-
 nodes.push({id: 5, label: '0x5', image:DIR+'switch.jpg', shape: 'image'});
-
-nodes.push({id: 1, label: '0x1', image:DIR+'switch.jpg', shape: 'image'});
-
-edges.push({from: 2, to: 1});
-
-edges.push({from: 3, to: 2});
-
-edges.push({from: 5, to: 6});
-
-edges.push({from: 1, to: 4});
-
-edges.push({from: 5, to: 4});
-
-edges.push({from: 3, to: 5});
-
-edges.push({from: 5, to: 1});
 
