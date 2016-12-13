c52870a20eddb49ed70f447227c9409f16a83695
d-miura <d-miura@ist.osaka-u.ac.jp>
2016-12-13 23:35:33 +0900

    push

--- output/topology.js
+++ output/topology.js
@@ -7,7 +7,29 @@ edges = [];
 var DIR = './images/';
 // Create a data table with nodes.
 
-nodes.push({id: 18764998447377, label: '11:11:11:11:11:11', image:DIR+'host.png', shape: 'image'});
+nodes.push({id: 5, label: '0x5', image:DIR+'switch.jpg', shape: 'image'});
 
-edges.push({from: 18764998447377, to: 1});
+nodes.push({id: 1, label: '0x1', image:DIR+'switch.jpg', shape: 'image'});
+
+nodes.push({id: 6, label: '0x6', image:DIR+'switch.jpg', shape: 'image'});
+
+nodes.push({id: 2, label: '0x2', image:DIR+'switch.jpg', shape: 'image'});
+
+nodes.push({id: 4, label: '0x4', image:DIR+'switch.jpg', shape: 'image'});
+
+nodes.push({id: 3, label: '0x3', image:DIR+'switch.jpg', shape: 'image'});
+
+edges.push({from: 1, to: 4});
+
+edges.push({from: 1, to: 2});
+
+edges.push({from: 1, to: 5});
+
+edges.push({from: 5, to: 3});
+
+edges.push({from: 5, to: 6});
+
+edges.push({from: 5, to: 4});
+
+edges.push({from: 2, to: 3});
 
